1da9cc0e44a04bc9749ce48db7208b4d89095e02
aimansae <saeedaimann@gmail.com>
2022-07-29 15:21:08 +0000

    Add validation to check that the values provided are integers

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ def get_sales_data():
     Get sales figures input from the user
     """
     print('Please enter salee data from last market')
-    print('Data should be six numbers')
+    print('Data should be six numbers separated by comma')
     print('Example: 10,20,30,40,50,60\n')
 
     data_str = input('Enter your data here:')
@@ -33,6 +33,7 @@ def validate_data(values):
     Raises valueError ig strings can not be converted into int, or if there are not 6 values
     """
     try:
+        [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f'Neded 6 values, you provided {len(values)}'
