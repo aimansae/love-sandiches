73143f6f2714645abad6f3fabea10d8e6cc84951
aimansae <saeedaimann@gmail.com>
2022-07-29 19:24:17 +0000

    Add function to retrieve the last 5 items from sales column

--- run.py
+++ run.py
@@ -94,6 +94,22 @@ def calculate_surplus_data(sales_row):
 
     return surplus_data
 
+def get_last_five_sales():
+    '''
+    Collects column of data from sales worksheet, taking
+    the last 5 entries for each sandwich and returns the 
+    sara as a list 
+    '''
+    sales= SHEET.worksheet('sales')
+   
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    return columns
+
+
 
 def main():
     '''
@@ -109,4 +125,6 @@ def main():
 
 
 print('welcome to love sandwiches data automation')
-main()
+#main()
+
+sales_columns = get_last_five_sales()
